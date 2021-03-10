a07f25967c220f6c9dfa6204748c14867a49ac62
Clayon Stewart <c.stewart89@gmail.com>
2021-03-10 00:37:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+
+class School
+  
+  #ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+   ROSTER
+  end
+  
+  def add_student(name, grade)
+     key = ROSTER.keys.find { |i| i == grade }
+     puts key
+    if key
+      ROSTER[grade] << name
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << name
+    end
+  end
+  
+  def grade(grade_num)
+    ROSTER.select do |key, value|
+      if key == grade_num
+        value
+      end
+    end
+  end
+  
+  
+  def sort
+    ROSTER.sort
+  end
+    
+end
